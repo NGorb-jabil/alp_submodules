ab0da31527524c033f7938874c49e1dc2af68642
ngorb <mykola_horb@jabil.com>
2022-07-11 20:29:55 +0300

    Add libs folder with submodules
    Change-Id: Ib17e36558737d05e55f18b378d8ea424e7cd7bdf

---
+++ libs/mtb.mk
@@ -0,0 +1,18 @@
+################################################
+# Auto-generated as part of running getlibs 
+################################################
+
+# List of local libraries 
+SEARCH_abstraction-rtos=./libs/abstraction-rtos
+SEARCH_clib-support=./libs/clib-support
+SEARCH_core-lib=./libs/core-lib
+SEARCH_core-make=./libs/core-make
+SEARCH_freertos=./libs/freertos
+SEARCH_mtb-hal-cat1=./libs/mtb-hal-cat1
+SEARCH_mtb-pdl-cat1=./libs/mtb-pdl-cat1
+SEARCH_psoc6cm0p=./libs/psoc6cm0p
+SEARCH_recipe-make-cat1a=./libs/recipe-make-cat1a
+SEARCH_retarget-io=./libs/retarget-io
+SEARCH_TARGET_PSOC6-GENERIC=./libs/TARGET_PSOC6-GENERIC
+
+-include $(CY_INTERNAL_APP_PATH)/importedbsp.mk
